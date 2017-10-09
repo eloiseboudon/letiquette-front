cac61a1f6ce26d08c34a609b8df296101c0aaffd
Unknown <boudon.eloise@gmail.com>
2017-10-09 12:10:38 +0200

    right top responsive

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -11,21 +11,25 @@ export class NavbarComponent {
 
   ngOnInit(): void {
     $(window).scroll(function () { 
-        if ($(this).scrollTop() > 120) {      
-            document.getElementById("top-page-scroll").style.visibility = "visible";
-            document.getElementById("top-page").style.visibility = "hidden";
-        } else {    
-          document.getElementById("top-page-scroll").style.visibility = "hidden";
-          document.getElementById("top-page").style.visibility = "visible";
-        }
+      if ($(this).scrollTop() > 120) {      
+          document.getElementById("top-page-scroll").style.visibility = "visible";
+          document.getElementById("top-page").style.visibility = "hidden";
+      } else {    
+        document.getElementById("top-page-scroll").style.visibility = "hidden";
+        document.getElementById("top-page").style.visibility = "visible";
+      }
     });
+
+    // $(window).resize(function (){
+    //   if(window.innerWidth < 1260) {
+    //     var element = document.getElementById("top-right-name");
+    //     // element.style.visibility = "hidden";
+    //     element.parentElement.removeChild(element);
+    //   }else{
+
+    //   }
+    // }) ; 
+  
+  
   }
-
-
-
- 
-
-
-
-
 }
