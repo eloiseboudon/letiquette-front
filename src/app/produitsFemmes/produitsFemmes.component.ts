08491cb4439436a861666e920299dad18f6f66b4
Unknown <boudon.eloise@gmail.com>
2017-10-31 15:06:07 +0100

    filter prix

--- src/app/produitsFemmes/produitsFemmes.component.ts
+++ src/app/produitsFemmes/produitsFemmes.component.ts
@@ -55,6 +55,38 @@ export class ProduitsFemmesComponent implements OnInit {
         this.getAllTailleType();
         this.getFamilleBySexe();
         // this.getAllFemmesFournisseurs();
+        this.rangeSlide();
+
+        
+    }
+    getVals(){
+        // Get slider values
+        // var parent = this.parentNode;
+        var slides = document.getElementsByTagName("input");
+        var slide1 = parseFloat( slides[0].value );
+        var slide2 = parseFloat( slides[1].value );
+    
+        console.log(slide1 +" "+ slide2);
+        // Neither slider will clip the other, so make sure we determine which is larger
+        if( slide1 > slide2 ){ var tmp = slide2; slide2 = slide1; slide1 = tmp; }
+        
+        var displayElement = document.getElementsByClassName("rangeValues")[0];
+            displayElement.innerHTML = "$ " + slide1 + "k - $" + slide2 + "k";
+      }
+
+
+    rangeSlide(){
+        var sliderSections = document.getElementsByClassName("range-slider");
+        for (var x = 0; x < sliderSections.length; x++) {
+            var sliders = sliderSections[x].getElementsByTagName("input");
+            for (var y = 0; y < sliders.length; y++) {
+                if (sliders[y].type === "range") {
+                    sliders[y].oninput = this.getVals;
+                    // Manually trigger event first time to display values
+                    // sliders[y].oninput();
+                }
+            }
+        }
     }
 
 
